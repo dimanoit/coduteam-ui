a2a7a3d9c1e87a81fc646af34c6a8bab0f55f117
dimanoit <dimanoit@gmail.com>
2023-11-11 15:46:46 +0100

    add images for home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,16 +18,20 @@ export class HomeComponent {
       title: 'Project Discovery',
       description:
         'Easily browse and explore a wide variety of projects in different domains and technologies.',
+      imageSrc: 'project_discovery.png',
     },
+
     {
       title: 'Team Finding',
       description:
         'Connect with other developers who share your interests and collaborate on exciting projects.',
+      imageSrc: 'team_finding.png',
     },
     {
       title: 'Skill Development',
       description:
         'Improve your coding skills through hands-on experience and learning from experienced mentors.',
+      imageSrc: 'skill_development.png',
     },
   ];
 
@@ -36,16 +40,23 @@ export class HomeComponent {
       title: 'Networking',
       description:
         'Connect with like-minded developers and professionals, expanding your professional network.',
+      imageSrc: '',
     },
     {
       title: 'Experience',
       description:
         'Work on real-world projects, gaining hands-on experience and enhancing your skillset.',
+      imageSrc: '',
     },
   ];
+
+  getImageScr(item: CarouselItem): string {
+    return '/assets/' + item.imageSrc;
+  }
 }
 
 interface CarouselItem {
   title: string;
   description: string;
+  imageSrc: string;
 }
