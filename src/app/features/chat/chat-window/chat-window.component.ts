c92681ac235b16017a24bf6a68247dcb86f0e712
Z1rael88 <oleksiibahmet@gmail.com>
2024-03-25 08:30:17 +0100

    chat-messages-containers

--- src/app/features/chat/chat-window/chat-window.component.ts
+++ src/app/features/chat/chat-window/chat-window.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ScrollTopModule } from 'primeng/scrolltop';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
 
 interface Message {
   username: string;
   text: string;
 }
-
 @Component({
   selector: 'app-chat-window',
+  standalone: true,
+  imports: [CommonModule, FormsModule, ScrollTopModule,ScrollPanelModule],
   templateUrl: './chat-window.component.html',
   styleUrls: ['./chat-window.component.scss'],
 })
@@ -18,6 +24,13 @@ export class ChatWindowComponent {
     if (this.newMessage.trim() !== '') {
       this.messages.push({ username: 'You', text: this.newMessage });
       this.newMessage = '';
+    }
+  }
+  onEnterPress(event: Event) {
+    const keyboardEvent = event as KeyboardEvent;
+    if (keyboardEvent.key == 'Enter' && !keyboardEvent.shiftKey) {
+      event.preventDefault();
+      this.sendMessage();
     }
   }
 }
